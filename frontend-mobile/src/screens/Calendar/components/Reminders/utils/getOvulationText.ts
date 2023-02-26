54523b0e30357346c2e75ab2d9b81e7e775e1bc9
Aleksandar Vikor <a.vikor@vegait.rs>
2023-02-26 13:33:33 +0100

    Fix ovulation reminder.

--- frontend-mobile/src/screens/Calendar/components/Reminders/utils/getOvulationText.ts
+++ frontend-mobile/src/screens/Calendar/components/Reminders/utils/getOvulationText.ts
@@ -1,5 +1,5 @@
 import { getOvulationDate } from '../../../utils';
-import { addDays, isAfter, isToday } from 'date-fns';
+import { addDays, isAfter, isToday, subDays } from 'date-fns';
 import { getOvulationUpcomingText } from './getOvulationUpcomingText';
 
 const FALLBACK = 'Podsetnik za početak ovulacije.';
@@ -8,12 +8,24 @@ export const getOvulationText = (menstruationStart?: string, cycleLength?: numbe
   if (!menstruationStart || cycleLength === undefined) {
     return FALLBACK;
   }
-
+  const Today = new Date();
+  console.log('MENSTRUATION START', menstruationStart);
   const ovulationDate = getOvulationDate(menstruationStart, cycleLength);
+  const previousOvulationDate = subDays(ovulationDate, cycleLength);
+  console.log('PREV', previousOvulationDate);
 
-  if (isAfter(ovulationDate, new Date()) || isToday(ovulationDate)) {
+  if (isAfter(previousOvulationDate, Today) || isToday(previousOvulationDate)) {
+    return getOvulationUpcomingText(previousOvulationDate);
+  }
+
+  if (isAfter(ovulationDate, Today) || isToday(ovulationDate)) {
+    console.log('[1]');
+    console.log('isAfter', isAfter(ovulationDate, new Date()));
+    console.log('isToday', isToday(ovulationDate));
+    console.log('ovulationDate', ovulationDate);
     return getOvulationUpcomingText(ovulationDate);
   }
   const upcomingOvulation = addDays(ovulationDate, cycleLength);
+  console.log('[2]');
   return getOvulationUpcomingText(upcomingOvulation);
 };
