c0e6e324a970988697fd8c6bcd766bcbd70fa8df
s.stankov <s.stankov@vegait.rs>
2023-05-24 11:57:30 +0200

    Add apis for consumer

--- backend/app/core/models/consumer.py
+++ backend/app/core/models/consumer.py
@@ -1,8 +1,19 @@
+from django.contrib.auth.hashers import make_password
 from django.db import models
+from django.utils import timezone
+
 
 class Consumer(models.Model):
-    email = models.EmailField(unique=True)
-    name = models.CharField(max_length = 50)
+    name = models.CharField(max_length=150)
     date_of_birth = models.DateField()
-    enable_notifications = models.BooleanField(default=True)
-    is_active = models.BooleanField(default=True)
+    menstruation_length = models.PositiveIntegerField(default=0)
+    cycle_length = models.PositiveIntegerField(default=0)
+    menstruation_start_date = models.DateField(default=timezone.now)
+    email = models.EmailField(primary_key=True)
+    google_id = models.CharField(max_length=255, unique=True, default='')
+
+    def save(
+        self, force_insert=False, force_update=False, using=None, update_fields=None
+    ):
+        self.google_id = make_password(self.google_id)
+        super().save(force_insert=False, force_update=False, using=None, update_fields=None)
--- backend/app/pink_flag_calendar/models/calendar.py
+++ backend/app/pink_flag_calendar/models/calendar.py
@@ -4,5 +4,3 @@ from core.models.consumer import Consumer
 class Calendar(models.Model):
     last_period_length = models.IntegerField()
     last_cycle_length = models.IntegerField()
-
-    consumer = models.OneToOneField(Consumer, null=False, on_delete=models.CASCADE)
