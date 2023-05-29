5e7eea54d3f7772cd5a0cc36ed14756e7002915f
s.stankov <s.stankov@vegait.rs>
2023-05-29 11:00:50 +0200

    fix 401

--- backend/app/core/api_views.py
+++ backend/app/core/api_views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.hashers import check_password
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
+from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import GenericViewSet
 from core.models.consumer import Consumer
 from django.http.response import Http404
@@ -16,6 +17,7 @@ class ConsumerViewSet(
 ):
     queryset = Consumer.objects.all()
     serializer_class = ConsumerSerializer
+    permission_classes = [AllowAny]
 
     def get_object(self):
         o = self.queryset.filter(pk=self.kwargs['pk']).first()
